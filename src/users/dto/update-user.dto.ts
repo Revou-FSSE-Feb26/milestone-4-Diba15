bffcfce85a65826c184b8782d1f9baff0bb60f3d
Dimas Bagas Saputro <dimazzbagazz@yahoo.co.id>
2026-08-06 16:45:08 +0700

    refactor(user): update UpdateUserDto to omit id and password fields from CreateUserDto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,6 +1,7 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['email', 'password'] as const),
-) {}
+export class UpdateUserDto extends OmitType(CreateUserDto, [
+  'id',
+  'password',
+] as const) {}
